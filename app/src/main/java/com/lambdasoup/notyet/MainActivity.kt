87c2f774a561bc1e710a51ff8545372217ee77c4
Juliane Lehmann <juliane.lehmann@connected-health.eu>
2022-01-16 16:21:16 +0100

    Default Android project

---
+++ app/src/main/java/com/lambdasoup/notyet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lambdasoup.notyet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
