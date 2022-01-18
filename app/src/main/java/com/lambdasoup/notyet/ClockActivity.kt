62f1bc998829314c651abc2dd05b524ca63900a1
Juliane Lehmann <juliane.lehmann@connected-health.eu>
2022-01-18 17:15:34 +0100

    Introduce StartActivity to launch ClockActivity with configuration

--- app/src/main/java/com/lambdasoup/notyet/ClockActivity.kt
+++ app/src/main/java/com/lambdasoup/notyet/ClockActivity.kt
@@ -1,11 +1,58 @@
 package com.lambdasoup.notyet
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.ColorInt
+import java.time.LocalDateTime
 
 class ClockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clock)
+
+        findViewById<ClockView>(R.id.clock).apply {
+            (intent.getSerializableExtra(CLOSE_TO_TARGET_TIME) as LocalDateTime?)?.let {
+                closeToTargetTime = it
+            }
+            (intent.getSerializableExtra(TARGET_TIME) as LocalDateTime?)?.let {
+                targetTime = it
+            }
+            (intent.getIntExtra(DIAL_COLOR, -1)).takeIf { it != -1 }?.let {
+                dialColor = it
+            }
+            (intent.getIntExtra(FAR_FROM_TARGET_BG_COLOR, -1)).takeIf { it != -1 }?.let {
+                farFromTargetBackgroundColor = it
+            }
+            (intent.getIntExtra(CLOSE_TO_TARGET_BG_COLOR, -1)).takeIf { it != -1 }?.let {
+                closeToTargetBackgroundColor = it
+            }
+            (intent.getIntExtra(TARGET_BG_COLOR, -1)).takeIf { it != -1 }?.let {
+                targetBackgroundColor = it
+            }
+        }
+    }
+
+    companion object {
+        private const val CLOSE_TO_TARGET_TIME = "com.lambdasoup.notyet.ClockActivity_EXTRA_closeToTargetTime"
+        private const val TARGET_TIME = "com.lambdasoup.notyet.ClockActivity_EXTRA_targetTime"
+        private const val DIAL_COLOR = "com.lambdasoup.notyet.ClockActivity_EXTRA_dialColor"
+        private const val FAR_FROM_TARGET_BG_COLOR = "com.lambdasoup.notyet.ClockActivity_EXTRA_farFromTargetBgColor"
+        private const val CLOSE_TO_TARGET_BG_COLOR = "com.lambdasoup.notyet.ClockActivity_EXTRA_closeToTargetBgColor"
+        private const val TARGET_BG_COLOR = "com.lambdasoup.notyet.ClockActivity_EXTRA_targetBgColor"
+
+        fun getIntent(
+            context: Context, closeToTargetTime: LocalDateTime? = null, targetTime: LocalDateTime? = null,
+            @ColorInt dialColor: Int = -1, @ColorInt farFromTargetBgColor: Int = -1, @ColorInt closeToTargetBgColor: Int = -1,
+            @ColorInt targetBgColor: Int = -1
+        ): Intent =
+            Intent(context, ClockActivity::class.java)
+                .putExtra(CLOSE_TO_TARGET_TIME, closeToTargetTime)
+                .putExtra(TARGET_TIME, targetTime)
+                .putExtra(DIAL_COLOR, dialColor)
+                .putExtra(FAR_FROM_TARGET_BG_COLOR, farFromTargetBgColor)
+                .putExtra(CLOSE_TO_TARGET_BG_COLOR, closeToTargetBgColor)
+                .putExtra(TARGET_BG_COLOR, targetBgColor)
     }
 }
--- app/src/main/java/com/lambdasoup/notyet/StartActivity.kt
+++ app/src/main/java/com/lambdasoup/notyet/StartActivity.kt
@@ -1,4 +1,29 @@
 package com.lambdasoup.notyet
 
-class StartActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.toColorInt
+import com.google.android.material.button.MaterialButton
+import java.time.LocalDateTime
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_start)
+
+        findViewById<MaterialButton>(R.id.start_button).setOnClickListener {
+            startActivity(
+                ClockActivity.getIntent(
+                    applicationContext,
+                    closeToTargetTime = LocalDateTime.now().plusMinutes(1),
+                    targetTime = LocalDateTime.now().plusMinutes(2),
+                    dialColor = "#FF888888".toColorInt(),
+                    farFromTargetBgColor = "#FF00FF88".toColorInt(),
+                    closeToTargetBgColor = "#FFAAAA00".toColorInt(),
+                    targetBgColor = "#FFAA00AA".toColorInt(),
+                )
+            )
+        }
+    }
 }
